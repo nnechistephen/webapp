72e55dbd6596d347b1ae0b1d3ee0cfbb0b318af0
Markachi <46232083+Markachi@users.noreply.github.com>
2019-09-05 13:30:34 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<head>
+	<title>Web Blog</title>
+	<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script type="text/javascript" href = "js/bootstrap.min.js"></script>
+	<script type="text/javascript" href = "js/jquery.min.js"></script>
+	<script type="text/javascript" href = "js/popper.min.js"></script>
+</head>
+
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class = "col">
+				<?php include 'header.php'; ?> <!--This is header-->
+	    	</div>
+	    </div>
+
+	    <div class="row">
+	    	<div class = "col">
+	    		<p>content goes here</p>
+	    	</div>
+	    </div>
+
+		<div class="row">
+			<div class="col">
+				<?php include 'footer.php';?>
+			</div>
+		</div>	
+	</div>	
+</body>
